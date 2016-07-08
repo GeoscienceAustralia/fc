c2756410557838f0763b1f2ae6d95ba847312266
andrewdhicks <andrew.hicks@ga.gov.au>
2016-07-08 17:28:13 +1000

    Use the git version to set the python package version

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ Setup
 
 This compiles all the Fortran extensions.
 """
+import os
 
 from setuptools import setup, find_packages
 
 setup(name='fc',
-      version='2.0',
+      version=os.environ.get('version', 0.0),
       description='Geoscience Australia - Fractional Cover for AGDC',
       long_description=open('README.md', 'r').read(),
       license='Apache License 2.0',
