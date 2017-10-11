21b717f4664f95983ecfb6bc9a9d18dc56405d80
Jeremy Hooke <jez@stulk.com>
2017-10-11 15:37:09 +1100

    Consistent naming of task descriptions

--- fc/fc_app.py
+++ fc/fc_app.py
@@ -256,7 +256,7 @@ def submit(index: Index,
     output_type = app_config['output_type']
     source_type = app_config['source_type']
 
-    task_description, task_description_path = init_task_app(
+    task_desc, task_path = init_task_app(
         job_type="fc",
         source_types=[source_type],
         output_types=[output_type],
@@ -265,7 +265,7 @@ def submit(index: Index,
         pbs_project=project,
         pbs_queue=queue
     )
-    _LOG.info("Created task description: %s", task_description_path)
+    _LOG.info("Created task description: %s", task_path)
 
     if no_qsub:
         _LOG.info('Skipping submission due to --no-qsub')
@@ -273,10 +273,10 @@ def submit(index: Index,
 
     job_id = submit_subjob(
         name='generate',
-        task_description=task_description,
+        task_desc=task_desc,
         command=[
             'generate', '-v', '-v',
-            '--task-description', str(task_description_path),
+            '--task-desc', str(task_path),
             '--tag', tag
         ],
         qsub_params=dict(
@@ -292,7 +292,7 @@ def submit(index: Index,
 @cli.command(help='Generate Tasks into file and Queue PBS job to process them')
 @click.option('--no-qsub', is_flag=True, default=False, help="Skip submitting qsub for next step")
 @click.option(
-    '--task-description', 'task_description_file', help='',
+    '--task-desc', 'task_desc_file', help='Task environment description file',
     required=True,
     type=click.Path(exists=True, readable=True, writable=False, dir_okay=False)
 )
@@ -301,17 +301,17 @@ def submit(index: Index,
 @ui.log_queries_option
 @ui.pass_index(app_name=APP_NAME)
 def generate(index: Index,
-             task_description_file: str,
+             task_desc_file: str,
              no_qsub: bool,
              tag: str):
     _LOG.info('Tag: %s', tag)
 
-    config, task_description = _read_config_and_description(index, Path(task_description_file))
+    config, task_desc = _make_config_and_description(index, Path(task_desc_file))
 
     num_tasks_saved = task_app.save_tasks(
         config,
-        make_fc_tasks(index, config, query=task_description.parameters.query),
-        task_description.runtime_state.task_serialisation_path
+        make_fc_tasks(index, config, query=task_desc.parameters.query),
+        task_desc.runtime_state.task_serialisation_path
     )
     _LOG.info('Found %d tasks', num_tasks_saved)
 
@@ -329,12 +329,12 @@ def generate(index: Index,
 
     job_id = submit_subjob(
         name='run',
-        task_description=task_description,
+        task_desc=task_desc,
 
         command=[
             'run',
             '-vv',
-            '--task-description', str(task_description_file),
+            '--task-desc', str(task_desc_file),
             '--celery', 'pbs-launch',
             '--tag', tag,
         ],
@@ -348,11 +348,11 @@ def generate(index: Index,
     )
 
 
-def _read_config_and_description(index: Index, task_description_path: Path) -> Tuple[dict, TaskDescription]:
-    task_description = serialise.load_structure(task_description_path, TaskDescription)
+def _make_config_and_description(index: Index, task_desc_path: Path) -> Tuple[dict, TaskDescription]:
+    task_desc = serialise.load_structure(task_desc_path, TaskDescription)
 
-    task_time: datetime = task_description.task_dt
-    app_config = task_description.runtime_state.config_path
+    task_time: datetime = task_desc.task_dt
+    app_config = task_desc.runtime_state.config_path
 
     config = paths.read_document(app_config)
 
@@ -362,13 +362,13 @@ def _read_config_and_description(index: Index, task_description_path: Path) -> T
     config['app_config_file'] = Path(app_config).name
     config = make_fc_config(index, config)
 
-    return config, task_description
+    return config, task_desc
 
 
 @cli.command(help='Actually process generated task file')
 @click.option('--dry-run', is_flag=True, default=False, help='Check if output files already exist')
 @click.option(
-    '--task-description', 'task_description_file', help='',
+    '--task-desc', 'task_desc_file', help='Task environment description file',
     required=True,
     type=click.Path(exists=True, readable=True, writable=False, dir_okay=False)
 )
@@ -381,16 +381,16 @@ def _read_config_and_description(index: Index, task_description_path: Path) -> T
 def run(index,
         dry_run: bool,
         tag: str,
-        task_description_file: str,
+        task_desc_file: str,
         qsub: QSubLauncher,
         runner: TaskRunner,
         *args, **kwargs):
     _LOG.info('Starting Fractional Cover processing...')
     _LOG.info('Tag: %r', tag)
 
-    task_description = serialise.load_structure(Path(task_description_file), TaskDescription)
+    task_desc = serialise.load_structure(Path(task_desc_file), TaskDescription)
 
-    config, tasks = task_app.load_tasks(task_description.runtime_state.task_serialisation_path)
+    config, tasks = task_app.load_tasks(task_desc.runtime_state.task_serialisation_path)
 
     if dry_run:
         task_app.check_existing_files((task['filename'] for task in tasks))
@@ -400,7 +400,7 @@ def run(index,
     process_func = partial(process_result, index)
 
     try:
-        runner(task_description, tasks, task_func, process_func)
+        runner(task_desc, tasks, task_func, process_func)
         _LOG.info("Runner finished normally, triggering shutdown.")
     finally:
         runner.stop()
