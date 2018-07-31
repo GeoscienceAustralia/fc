cab8ca6c7c704e7b62d41cd986c5b1cf4dfdabad
Damien Ayers <damien@omad.net>
2018-07-31 22:09:55 +1000

    fix linting

--- check-code.sh
+++ check-code.sh
@@ -4,11 +4,11 @@
 set -eu
 set -x
 
-pep8 tests --max-line-length 120
+pycodestyle tests --max-line-length 120
 
 pylint -j 2 --ignore-patterns='.+\.so' --reports no fc
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
-py.test -r sx --cov fc --durations=5 fc tests $@
+pytest -r sx --cov fc --durations=5 fc tests $@
 
