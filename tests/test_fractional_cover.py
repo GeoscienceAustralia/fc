abe945e83ccef9df1b7f64c8fb4b501ea4d40387
Andrew Hicks <andrew.hicks@ga.gov.au>
2019-04-26 10:35:49 +1000

    Add test

--- tests/test_fractional_cover.py
+++ tests/test_fractional_cover.py
@@ -35,7 +35,35 @@ def test_fractional_cover(sr_filepath, fc_filepath):
     assert validation_ds.equals(fc_dataset)
 
 
-def open_dataset(file_path):
-    ds = xr.open_dataset(file_path, mask_and_scale=False, drop_variables='crs')
+def test_fractional_cover_lazy(sr_filepath, fc_filepath):
+    print(sr_filepath)
+    print(fc_filepath)
+
+    sr_dataset = open_dataset(sr_filepath, chunks={'x': 50, 'y': 50})
+
+    measurements = [
+        Measurement(name='PV', dtype='int8', nodata=-1, units='percent'),
+        Measurement(name='NPV', dtype='int8', nodata=-1, units='percent'),
+        Measurement(name='BS', dtype='int8', nodata=-1, units='percent'),
+        Measurement(name='UE', dtype='int8', nodata=-1, units='1'),
+    ]
+
+    fc_dataset = fractional_cover(sr_dataset, measurements)
+
+    assert fc_dataset.data.dask
+
+    fc_dataset.load()
+
+    assert set(fc_dataset.data_vars.keys()) == {m['name'] for m in measurements}
+
+    validation_ds = open_dataset(fc_filepath)
+
+    assert validation_ds == fc_dataset
+
+    assert validation_ds.equals(fc_dataset)
+
+
+def open_dataset(file_path, **kwargs):
+    ds = xr.open_dataset(file_path, mask_and_scale=False, drop_variables='crs',  **kwargs)
     ds.attrs['crs'] = datacube.utils.geometry.CRS('EPSG:32754')
     return ds
