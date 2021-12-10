d255136bbb183f2ac4a90cd60127f9b56fcb49b5
Alex Leith <alexgleith@gmail.com>
2021-12-10 12:20:29 +1100

    Refactor to make the clipping after applying regression coefficients optional

--- fc/virtualproduct.py
+++ fc/virtualproduct.py
@@ -1,34 +1,14 @@
 import xarray as xr
+from datacube.virtual import Measurement, Transformation
 
-from datacube.virtual import Transformation, Measurement
 from fc import __version__
 from fc.fractional_cover import fractional_cover
 
 FC_MEASUREMENTS = [
-    {
-        'name': 'pv',
-        'dtype': 'int8',
-        'nodata': -1,
-        'units': 'percent'
-    },
-    {
-        'name': 'npv',
-        'dtype': 'int8',
-        'nodata': -1,
-        'units': 'percent'
-    },
-    {
-        'name': 'bs',
-        'dtype': 'int8',
-        'nodata': -1,
-        'units': 'percent'
-    },
-    {
-        'name': 'ue',
-        'dtype': 'int8',
-        'nodata': -1,
-        'units': ''
-    },
+    {"name": "pv", "dtype": "int8", "nodata": -1, "units": "percent"},
+    {"name": "npv", "dtype": "int8", "nodata": -1, "units": "percent"},
+    {"name": "bs", "dtype": "int8", "nodata": -1, "units": "percent"},
+    {"name": "ue", "dtype": "int8", "nodata": -1, "units": ""},
 ]
 
 
@@ -38,14 +18,17 @@ class FractionalCover(Transformation):
     Requires bands named 'green', 'red', 'nir', 'swir1', 'swir2'
     """
 
-    def __init__(self, regression_coefficients=None, c2_scaling=False, test_mode=False):
-        if regression_coefficients is None:
-            regression_coefficients = {band: [0, 1]
-                                       for band in ['green', 'red', 'nir', 'swir1', 'swir2']
-                                       }
+    def __init__(
+        self,
+        regression_coefficients=None,
+        c2_scaling=False,
+        test_mode=False,
+        clip_after_regression=False,
+    ):
         self.regression_coefficients = regression_coefficients
         self.c2_scaling = c2_scaling
         self.test_mode = test_mode
+        self.clip_after_regression = clip_after_regression
 
     @property
     def measurements(self):
@@ -62,26 +45,34 @@ class FractionalCover(Transformation):
         fc = []
 
         for time_idx in range(len(data.time)):
-            fc.append(fractional_cover(data.isel(time=time_idx), self.measurements, self.regression_coefficients))
-        fc = xr.concat(fc, dim='time')
-        fc.attrs['crs'] = data.attrs['crs']
+            fc.append(
+                fractional_cover(
+                    data.isel(time=time_idx),
+                    self.measurements,
+                    self.regression_coefficients,
+                    clip_after_regression=self.clip_after_regression
+                )
+            )
+        fc = xr.concat(fc, dim="time")
+        fc.attrs["crs"] = data.attrs["crs"]
         try:
-            fc = fc.rename(BS='bs', PV='pv', NPV='npv', UE='ue')
+            fc = fc.rename(BS="bs", PV="pv", NPV="npv", UE="ue")
         except ValueError:  # Assuming the names are already correct and don't need to be changed.
             pass
         return fc
 
     def algorithm_metadata(self):
         return {
-            'algorithm': {
-                'name': 'Fractional Cover',
-                'version': __version__,
-                'repo_url': 'https://github.com/GeoscienceAustralia/fc.git',
-                'parameters': {
-                    'regression_coefficients': self.regression_coefficients,
-                    'usgs_c2_scaling': self.c2_scaling
-                }
-            }}
+            "algorithm": {
+                "name": "Fractional Cover",
+                "version": __version__,
+                "repo_url": "https://github.com/GeoscienceAustralia/fc.git",
+                "parameters": {
+                    "regression_coefficients": self.regression_coefficients,
+                    "usgs_c2_scaling": self.c2_scaling,
+                },
+            }
+        }
 
 
 class FakeFractionalCover(FractionalCover):
@@ -95,15 +86,19 @@ class FakeFractionalCover(FractionalCover):
         if self.c2_scaling:
             # The C2 data need to be scaled
             data = scale_usgs_collection2(data)
-        return xr.Dataset({'pv': data.red,
-                           'bs': data.red,
-                           'npv': data.green},
-                          attrs=data.attrs)
+        return xr.Dataset(
+            {"pv": data.red, "bs": data.red, "npv": data.green}, attrs=data.attrs
+        )
 
 
 def scale_usgs_collection2(data):
-    return data.apply(scale_and_clip_dataarray, keep_attrs=True,
-                      scale_factor=0.275, add_offset=-2000, clip_range=(0, 10000))
+    return data.apply(
+        scale_and_clip_dataarray,
+        keep_attrs=True,
+        scale_factor=0.275,
+        add_offset=-2000,
+        clip_range=(0, 10000),
+    )
 
 
 def scale_and_clip_dataarray(
